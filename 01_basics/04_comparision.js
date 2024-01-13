9030150bcd1a073f33004789de922366c275b61e
Avinash Shrestha <151823312+avshrestha007@users.noreply.github.com>
2024-01-13 09:19:05 +0530

    Create 04_comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// Comparision Operator
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// Comparision between two data type
+
+console.log("2" > 0);
+console.log("02" > 0);
+
+/* Avoid This Type Of Code
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+*/
+
+// Strictly Comparision
+
+console.log("2" === 2);
