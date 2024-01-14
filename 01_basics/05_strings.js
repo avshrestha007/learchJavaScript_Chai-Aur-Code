e61e7765992111f74c9bddc5e47801dcb704bf6c
Avinash Shrestha <151823312+avshrestha007@users.noreply.github.com>
2024-01-14 11:13:46 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -14,3 +14,23 @@ console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0, 9)
+console.log(newString);
+
+const anotherString = gameName.slice(0, 1)
+console.log(anotherString);
+
+const newStringOne = "   avinash   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://avinash.com/avinash%20shrestha"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sunday'));
+
+const gameName1 = new String('Avinash-Shrestha-25')
+
+console.log(gameName1.split('-'));
