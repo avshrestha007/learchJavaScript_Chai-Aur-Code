64e59f2b174ef8c77c54ba37bedb90c52741c2b0
Avinash Shrestha <151823312+avshrestha007@users.noreply.github.com>
2024-01-14 10:54:50 +0530

    Create 05_strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "Avinash"
+const age = 26
+
+// console.log(name + age + " Dev");
+
+console.log(`Hello my name is ${name} and my is ${age}`);
+
+const gameName = new String('AvinashShrestha')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('n'));
