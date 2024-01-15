ac63c93cf7b9a34b8ce1b206d48235d37779a55d
Avinash Shrestha <151823312+avshrestha007@users.noreply.github.com>
2024-01-15 11:15:37 +0530

    Update 06_numsAndMaths.js

--- 01_basics/06_numsAndMaths.js
+++ 01_basics/06_numsAndMaths.js
@@ -1,30 +1,35 @@
 const score = 400
-// console.log(score);
+console.log(score);
 
 const balance = new Number(400)
-// console.log(balance);
+console.log(balance);
 
-// console.log(balance.toString());
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
 
 const otherNumber = 23.879
 
-// console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(3));
 
 const humdreds = 1000000
-// console.log(humdreds.toLocaleString('en-IN'));
+console.log(humdreds.toLocaleString('en-IN'));
 
 // ++++++++++++++++++ Maths ++++++++++++++++++++++
 
-// console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.5));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(23, 56,24,66,3,6));
-// console.log(Math.max(34,56,25,67,25,78,99));
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(23, 56,24,66,3,6));
+console.log(Math.max(34,56,25,67,25,78,99));
 
 console.log(Math.random());
 console.log((Math.random()*10));
 console.log((Math.random()*10) + 1);
+
+const min = 1
+const max = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
