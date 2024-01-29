624adc8f95026eb70b6fa71440a4ad1fb9419dd1
Avinash Shrestha <151823312+avshrestha007@users.noreply.github.com>
2024-01-29 09:41:31 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,4 +33,34 @@ function loginUserMessage(username){
 }
 
 // console.log(loginUserMessage("Avinash"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(12, 23, 34, 45, 56));
+
+const user = {
+    username: "Avinash",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+// handleObject({
+//     username: "Rishabh",
+//     price: 399
+// })
+
+const myNewArray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 400, 500]));
