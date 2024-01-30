8951931e9906110094b79fdb7fb680d608a6bb01
Avinash Shrestha <151823312+avshrestha007@users.noreply.github.com>
2024-01-30 09:47:40 +0530

    Update 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,3 +9,42 @@ if(true){
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+    const username = "Avinash"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if(true){
+    const firstName = "Avinash"
+    if(true){
+        const lastName = " Shresth"
+        // console.log(firstName + lastName);
+    }
+    // console.log(lastName);
+}
+
+// console.log(firstName);
+
+// ++++++++++++++++++++ interesting ++++++++++++++++++++
+
+console.log(addone(3));
+
+function addone(num) {
+    return num + 1
+}
+
+// console.log(addtwo(5));
+
+const addtwo = function(num){
+    return num + 2
+}
