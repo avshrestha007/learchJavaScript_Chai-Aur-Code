076b0f13da89decf7d1b3f395b62521a2864bd47
Avinash Shrestha <151823312+avshrestha007@users.noreply.github.com>
2024-02-03 10:18:30 +0530

    Update one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -3,11 +3,48 @@
 const isUserLoggedIn = true
 const temp = 100
 
-if (temp <= 50){
-    console.log("Temperature is less then 50");
-} else {
-    console.log("Temperature is greater the 50");
+// if (temp <= 50){
+//     console.log("Temperature is less then 50");
+// } else {
+//     console.log("Temperature is greater the 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+
+// console.log(`User Power: ${power}`);
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"), console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance <900) {
+//     console.log("less than 750");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const isUserLoggedIn1 = true
+const debitCard = true
+const isUserLoggedFromGoogle = false
+const isUserLoggedFromEmail = true
+
+
+if (isUserLoggedIn1 && debitCard && 2 == 2) {
+    console.log("Allow to buy course");
 }
 
-console.log("Execute");
-// <, >, <=, >=, ==, !=, ===, !==
+if (isUserLoggedFromGoogle || isUserLoggedFromEmail) {
+    console.log("User logged In");
+}
