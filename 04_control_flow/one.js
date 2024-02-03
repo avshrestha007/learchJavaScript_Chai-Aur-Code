e4f71a3ce220caff1604e2e62ba866e20b75bfa8
Avinash Shrestha <151823312+avshrestha007@users.noreply.github.com>
2024-02-03 10:01:04 +0530

    Update one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+// if
+
+const isUserLoggedIn = true
+const temp = 100
+
+if (temp <= 50){
+    console.log("Temperature is less then 50");
+} else {
+    console.log("Temperature is greater the 50");
+}
+
+console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
